e52b4529928fbdc89a928b386d5816aa7c678951
altuntasomer <altuntas.omer@yandex.com.tr>
2021-10-31 11:40:14 +0300

    List all files on Firebase

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
-print("hello")
-print("merhaba")
+import pyrebase
+
+firebase_config = {
+    
+    "serviceAccount": "serviceAccountKey.json"
+}
+
+firebase = pyrebase.initialize_app(firebase_config)
+storage = firebase.storage()
+
+ab=str(1)
+i = 0
+all_files = storage.list_files()
+
+for file in all_files:
+    i += 1
+    if i == 5:
+        break
+        exit()
+    try:
+            print(file.name)
+            z=storage.child(file.name).get_url(None)
+            storage.child(file.name).download("hello.jpg")
+            x=int(ab)
+            ab=str(x+1)
+    except:
+            print('Download Failed')
